74bf88c776b0746e4da2b3995de1b606e8b69cb6
Rodrigo Borges <rodrigo_craft@LIVE.COM>
2019-11-11 14:49:35 -0300

    Voltando ao arquivo original

--- TrabGrafos/Form1.Designer.cs
+++ TrabGrafos/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salvarComoImagemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salvarComoDadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,8 +49,7 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.arquivoToolStripMenuItem,
-            this.ajudaToolStripMenuItem});
+            this.arquivoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(784, 24);
@@ -88,12 +86,6 @@
             this.salvarComoDadoToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
             this.salvarComoDadoToolStripMenuItem.Text = "Salvar como Dado";
             // 
-            // ajudaToolStripMenuItem
-            // 
-            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
-            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.ajudaToolStripMenuItem.Text = "Ajuda";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -105,7 +97,7 @@
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Teste";
+            this.Text = "Gerador de Grafos";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.menuStrip1.ResumeLayout(false);
@@ -123,7 +115,6 @@
         private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salvarComoImagemToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salvarComoDadoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
     }
 }
 
